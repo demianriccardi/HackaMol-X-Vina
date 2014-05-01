a4c048642f0e42b6b7254b8a95c69fca1d01024b
Demian Riccardi <dde@ornl.gov>
2014-05-01 11:06:24 -0400

    BUILD size/center ops now in triggers

--- lib/HackaMol/X/Vina.pm
+++ lib/HackaMol/X/Vina.pm
@@ -17,13 +17,18 @@
   ) foreach qw(center_x center_y center_z size_x size_y size_z);
 
   has $_ => (
-      is => 'ro', isa => 'Math::Vector::Real', predicate => "has_$_",
-  ) foreach qw(center size);
-
-  has $_ => (
     is => 'ro', isa => 'Int', predicate => "has_$_",
   ) foreach qw(energy_range exhaustiveness seed cpu num_modes);
 
+  has 'center' => (
+      is => 'rw', isa => 'Math::Vector::Real', predicate => "has_center",
+      trigger => \&_set_center,
+  );
+
+  has 'size' => (
+      is => 'rw', isa => 'Math::Vector::Real', predicate => "has_size",
+      trigger => \&_set_size,
+  );
 
   sub BUILD {
     my $self = shift;
@@ -33,19 +38,19 @@
     }
 
 #  setting center via MVR overrides whatever is set via center_x etc.
-    if ($self->has_center){
-      my ($x,$y,$z) = @{$self->center};
-      $self->center_x($x);  
-      $self->center_y($y);  
-      $self->center_z($z);  
-    }
+#    if ($self->has_center){
+#      my ($x,$y,$z) = @{$self->center};
+#      $self->center_x($x);  
+#      $self->center_y($y);  
+#      $self->center_z($z);  
+#    }
 
-    if ($self->has_size){
-      my ($x,$y,$z) = @{$self->size};
-      $self->size_x($x); 
-      $self->size_y($y);
-      $self->size_z($z);
-    }
+#    if ($self->has_size){
+#      my ($x,$y,$z) = @{$self->size};
+#      $self->size_x($x); 
+#      $self->size_y($y);
+#      $self->size_z($z);
+#    }
     
     unless ( $self->has_command ) {
         return unless ( $self->has_exe );
@@ -53,6 +58,20 @@
         $self->command($cmd);
     }
     return;
+  }
+
+  sub _set_center {
+    my ($self,$center,$old_center) = @_;
+    $self->center_x($center->[0]);
+    $self->center_y($center->[1]);
+    $self->center_z($center->[2]);
+  }
+
+  sub _set_size {
+    my ($self,$size,$old_size) = @_;
+    $self->size_x($size->[0]);
+    $self->size_y($size->[1]);
+    $self->size_z($size->[2]);
   }
 
   #required methods
@@ -70,7 +89,15 @@
   }
 
   sub _build_map_out{
-    my $sub_cr = sub { return (@_) };
+    my $sub_cr = sub {     
+                      my $self = shift; 
+                      my $qr = qr/^\s+\d+\s+(-*\d+\.\d)/;
+                      my ($stdout,$sterr) = $self->capture_sys_command; 
+                      my @be = map { m/$qr/; $1 }
+                               grep{ m/$qr/ } 
+                               split ("\n",$stdout);  
+                      return (@be);
+                     };
     return $sub_cr;
   }
 
--- t/HackaMol-X-Vina-Mapping.t
+++ t/HackaMol-X-Vina-Mapping.t
@@ -16,21 +16,22 @@ use Cwd;
 
 my $cwd      = getcwd;
 my $receptor = path('t/lib/receptor.pdbqt'); 
-my $ligand1  = path('t/lib/ligand1.pdbqt' ); 
-my $ligand2  = path('t/lib/ligand2.pdbqt' ); 
+my $ligand   = path('t/lib/ligand.pdbqt' ); 
 my $rmol     = HackaMol->new(hush_read=>1)->read_file_mol($receptor);    
-my $lmol1    = HackaMol->new(hush_read=>1)->read_file_mol($ligand1);    
-my $lmol2    = HackaMol->new(hush_read=>1)->read_file_mol($ligand2);    
+my $lmol     = HackaMol->new(hush_read=>1)->read_file_mol($ligand);    
+my $ligout   = $ligand->basename;
+$ligout      =~ s/\.pdbqt/_new\.pdbqt/;       
 
 my $obj = HackaMol::X::Vina->new(
-        receptor       => $receptor->basename,
-        ligand         => $ligand1->basename,
+        receptor       => $receptor->absolute->stringify,  # needs coercion!
+        ligand         => $ligand->absolute->stringify,
         in_fn          => "conf.txt",
-        center         => V(0,1,2),
-        size           => V(20,20,20),
+        out_fn         => $ligout,
+        center         => V(6.865, 3.449, 85.230),
+        size           => V(10,10,10),
         cpu            => 1,
-        num_modes      => 2,
-        exhaustiveness => 8,
+        num_modes      => 5,
+        exhaustiveness => 1,
         exe            => '~/bin/vina',
         scratch        => 't/tmp',
         seed           => 314159,
@@ -38,6 +39,23 @@ my $obj = HackaMol::X::Vina->new(
 
 my $input = $obj->map_input;
 print $input;
+
+my @bes   = $obj->map_output;
+my @Be_expected = qw(
+  -6.6
+  -5.9
+  -5.8
+  -5.7
+  -5.1
+);
+
+is_deeply(\@bes, \@Be_expected, 'binding energies computed with vina');
+
+$obj->center( V( 18.073, -2.360, 90.288 ) );
+$input = $obj->map_input;
+print $input;
+@bes   = $obj->map_output;
+print $_ . "\n" foreach @bes;
 
 done_testing();
 
