7106a94c10ff25a3f8e45fd9c369867099317c75
Demian Riccardi <demianriccardi@gmail.com>
2014-05-04 22:50:03 -0400

    update synopsis

--- lib/HackaMol/X/Vina.pm
+++ lib/HackaMol/X/Vina.pm
@@ -189,37 +189,36 @@ __END__
 
 =head1 SYNOPSIS
 
-       use HackaMol;
-       use HackaMol::X::Vina;
-       use Math::Vector::Real;
-
-       my $receptor = "receptor.pdbqt";
-       my $rmol     = HackaMol -> new( hush_read=>1 ) -> read_file_mol( $receptor );
-
-       my @centers = map  {$_ -> xyz}
-                     grep {$_ -> name    eq "OH" }
-                     grep {$_ -> resname eq "TYR"} $rmol -> all_atoms;
-
-       foreach my $center ( @centers ){
-
-           my $vina = HackaMol::X::Vina -> new(
-               receptor       => $receptor,
-               ligand         => "ligand.pdbtq",
-               center         => $center,
-               size           => V( 20, 20, 20 ),
-               cpu            => 4,
-               exhaustiveness => 12,
-               exe            => '~/bin/vina',
-               scratch        => 'tmp',
-           );
-
-           my $mol = $vina->dock_mol(3); # fill mol with 3 binding configurations
-
-           printf ("Score: %6.1f\n", $mol->get_score($_) ) foreach (0 .. $mol->tmax);
-
-           $mol->print_pdb_ts([0 .. $mol->tmax]);
-
-       }
+         use HackaMol;
+         use HackaMol::X::Vina;
+         use Math::Vector::Real;
+  
+         my $receptor = "receptor.pdbqt";
+         my $rmol     = HackaMol -> new( hush_read=>1 ) -> read_file_mol( $receptor ); 
+  
+         my @centers = map  {$_ -> xyz}
+                       grep {$_ -> name    eq "OH" }
+                       grep {$_ -> resname eq "TYR"} $rmol -> all_atoms;
+  
+         foreach my $center ( @centers ){
+  
+             my $vina = HackaMol::X::Vina -> new(
+                 receptor       => $receptor,
+                 ligand         => "lig.pdbqt",
+                 center         => $center,
+                 size           => V( 20, 20, 20 ),
+                 cpu            => 4,
+                 exhaustiveness => 12,
+                 exe            => '~/bin/vina',
+             );
+  
+             my $mol = $vina->dock_mol(3); # fill mol with 3 binding configurations 
+  
+             printf ("Score: %6.1f\n", $mol->get_score($_) ) foreach (0 .. $mol->tmax);          
+  
+             $mol->print_pdb_ts([0 .. $mol->tmax]); 
+  
+         }
 
 
 =head1 DESCRIPTION
